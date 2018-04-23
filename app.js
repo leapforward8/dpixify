f083b832586d233537e0f21eb3a10e58d6dbfe4c
Sean Warren <sean.warren@gmail.com>
2018-04-23 15:35:38 -0400

    Updated Passport Version

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ passport.use(
   new Strategy(
     {
       authorizationURL: `https://steemconnect.com/oauth2/authorize`,
-      tokenURL: `https://steemconnect.com/oauth2/token`,
+      tokenURL: `https://steemconnect.com/api/oauth2/token`,
       clientID: process.env.clientID,
       clientSecret: process.env.clientSecret,
       callbackURL: `http://localhost:3000/auth/oauth/oauth2/callback`,
